84a0d8a2446d8e0a193ebb8e9f03c80a323eab12
Dagromani <109410300+Dagromani@users.noreply.github.com>
2022-07-17 22:15:10 +0300

    Update Task7.java

--- HW2/src/Task7.java
+++ HW2/src/Task7.java
@@ -3,23 +3,28 @@ import java.util.Random;
 
 public class Task7 {
     public static void main(String[] args) {
-        int[] array = new int[10];
+        //int[] array = new int[10];
         Random randInt = new Random();
 
-        for (int i = 0 ; i < 10 ; i++) {
-            int randNumber = randInt.nextInt(100);
-            array[i] = randNumber;
-        }
-        int maxNumber = 0;
+        //for (int i = 0 ; i < 10 ; i++) {
+        //    int randNumber = randInt.nextInt(100);
+        //    array[i] = randNumber;
+        //}
 
-        for (int x : array) {
-            System.out.println(x);
-            if (x > maxNumber & x % 2 == 0) {
-                maxNumber = x;
+        int[] array = {-1,-3,-6,-17,-276,-99,-103};
+
+        int maxNumber = Integer.MIN_VALUE;
+
+        for (int x = 0 ; x < array.length ; x++) {
+            //System.out.println(x);
+            if (array[x] % 2 == 0 && array[x] > maxNumber) {
+                maxNumber = array[x];
+
 
             }
 
         }
-        System.out.println("Max even number: " + maxNumber);
+
+            System.out.println("Max even number: " + maxNumber);
     }
 }
