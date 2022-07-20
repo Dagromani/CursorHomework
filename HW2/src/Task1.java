e3b0d23b4630ef11393afd0baaced97fd470093f
Dagromani <109410300+Dagromani@users.noreply.github.com>
2022-07-20 11:12:03 +0300

    Update Task1.java

--- HW2/src/Task1.java
+++ HW2/src/Task1.java
@@ -5,7 +5,7 @@ public class Task1 {
     public static void main(String[] args) {
         Random rand = new Random();
         int[] array = new int[10];
-        for (int i = 0 ; i < 10 ; i++) {
+        for (int i = 0; i < 10; i++) {
             int newRand = rand.nextInt(10000);
             if (newRand % 2 == 0) {
                 array[i] = newRand;
