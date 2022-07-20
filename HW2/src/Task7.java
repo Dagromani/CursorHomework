58514166c75e4ffcd0532481c74971ea642a11f6
Dagromani <109410300+Dagromani@users.noreply.github.com>
2022-07-20 11:14:41 +0300

    Update Task7.java

--- HW2/src/Task7.java
+++ HW2/src/Task7.java
@@ -1,22 +1,19 @@
-import java.sql.SQLOutput;
 import java.util.Random;
 
 public class Task7 {
     public static void main(String[] args) {
-        //int[] array = new int[10];
+        int[] array = new int[10];
         Random randInt = new Random();
 
-        //for (int i = 0 ; i < 10 ; i++) {
-        //    int randNumber = randInt.nextInt(100);
-        //    array[i] = randNumber;
-        //}
-
-        int[] array = {-1,-3,-6,-17,-276,-99,-103};
+        for (int i = 0; i < 10; i++) {
+            int randNumber = randInt.nextInt(100);
+            array[i] = randNumber;
+        }
 
         int maxNumber = Integer.MIN_VALUE;
 
-        for (int x = 0 ; x < array.length ; x++) {
-            //System.out.println(x);
+        for (int x = 0; x < array.length; x++) {
+
             if (array[x] % 2 == 0 && array[x] > maxNumber) {
                 maxNumber = array[x];
 
@@ -25,6 +22,8 @@ public class Task7 {
 
         }
 
-            System.out.println("Max even number: " + maxNumber);
+        System.out.println("Max even number: " + maxNumber);
+
+
     }
 }
