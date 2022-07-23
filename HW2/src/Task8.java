12545a7e5f499bb5a5deb45843813950ef8d86a5
Dagromani <109410300+Dagromani@users.noreply.github.com>
2022-07-23 16:36:38 +0300

    Update Task8.java

--- HW2/src/Task8.java
+++ HW2/src/Task8.java
@@ -4,17 +4,14 @@ public class Task8 {
     public static void main(String[] args) {
         int[] array = new int[10];
         Random randInt = new Random();
-
         for (int i = 0; i < 10; i++) {
             int randNumber = randInt.nextInt(100);
             array[i] = randNumber;
         }
-
         for (int x = 0; x < 10; x++) {
             if (x % 2 == 0) {
                 System.out.println(array[x]);
             }
-
         }
         System.out.println("");
         for (int y : array) {
