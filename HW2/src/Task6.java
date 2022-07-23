72e9d3a99df29733dc5bc39f69f8ba4e1f1a1ec8
Dagromani <109410300+Dagromani@users.noreply.github.com>
2022-07-23 16:36:12 +0300

    Update Task6.java

--- HW2/src/Task6.java
+++ HW2/src/Task6.java
@@ -4,23 +4,16 @@ public class Task6 {
     public static void main(String[] args) {
         int[] array = new int[10];
         Random randInt = new Random();
-
         for (int i = 0; i < 10; i++) {
             int randNumber = randInt.nextInt(100);
             array[i] = randNumber;
         }
-
         int sum = 0;
-
         for (int x : array) {
             sum = sum + x;
             System.out.println(x);
-
         }
-
-        int average = sum / array.length;
+        double average = (double)sum / array.length;
         System.out.println("Average : " + average);
-
-
     }
 }
