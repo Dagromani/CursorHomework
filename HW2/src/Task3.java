17f5aafa709644e02b823afc343fa1cee2bfbac0
Dagromani <109410300+Dagromani@users.noreply.github.com>
2022-07-23 16:35:21 +0300

    Update Task3.java

--- HW2/src/Task3.java
+++ HW2/src/Task3.java
@@ -4,15 +4,10 @@ public class Task3 {
     public static void main(String[] args) {
         char[] array = new char[10];
         Random r = new Random();
-
         for (int i = 0; i < 10; i++) {
             char randChar = (char) (r.nextInt(26) + 'a');
             array[i] = randChar;
             System.out.println(array[i]);
-
-
         }
-
-
     }
 }
