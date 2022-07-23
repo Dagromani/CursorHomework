fecdda2e393fcd17151e84fe753e4bea8c77f666
Dagromani <109410300+Dagromani@users.noreply.github.com>
2022-07-23 16:35:38 +0300

    Update Task4.java

--- HW2/src/Task4.java
+++ HW2/src/Task4.java
@@ -4,23 +4,17 @@ public class Task4 {
     public static void main(String[] args) {
         int[] array = new int[10];
         Random r = new Random();
-
         for (int i = 0; i < 10; i++) {
             int randInt = r.nextInt(100);
             array[i] = randInt;
         }
-
         int maxNumber = Integer.MIN_VALUE;
-
         for (int x = 0; x < 10; x++) {
             if (array[x] > maxNumber) {
                 maxNumber = array[x];
             }
             System.out.println(array[x]);
         }
-
-
         System.out.println("Max number : " + maxNumber);
-
     }
 }
