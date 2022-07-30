830190c0c0c679b76f60db4d2dc541b8697918d5
Dagromani <109410300+Dagromani@users.noreply.github.com>
2022-07-30 16:58:41 +0300

    Update DoProteinShake.java

--- HW 4/untitled/src/gymservices/DoProteinShake.java
+++ HW 4/untitled/src/gymservices/DoProteinShake.java
@@ -1,26 +1,26 @@
 package gymservices;
 
 public class DoProteinShake extends Service {
-
     private int volume;
-
-
-    public DoProteinShake(int price) {
-        super(price);
+    public DoProteinShake(int price, String name) {
+        super(price, name);
     }
-
+    @Override
+    public String getName() {
+        return super.getName();
+    }
+    @Override
+    public int getPrice() {
+        return super.getPrice();
+    }
     public int getVolume() {
         return volume;
     }
-
     public void setVolume(int volume) {
         this.volume = volume;
     }
-
     @Override
     public String toString() {
-        return "DoProteinShake{" +
-                "volume=" + volume +
-                '}';
+        return super.toString();
     }
 }
