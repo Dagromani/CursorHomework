bb2c5f316ba2710954a769ea08dda920ca7cf7f7
Dagromani <109410300+Dagromani@users.noreply.github.com>
2022-07-30 16:59:42 +0300

    Update Main.java

--- HW 4/untitled/src/gymservices/Main.java
+++ HW 4/untitled/src/gymservices/Main.java
@@ -2,17 +2,19 @@ package gymservices;
 
 public class Main {
     public static void main(String[] args) {
-        GroupTraining yoga = new GroupTraining(100, "hard");
+        GroupTraining yoga = new GroupTraining(100, "Yoga" , "medium");
         yoga.setDuration(60);
-
         System.out.println(yoga);
         System.out.println(yoga.getPrice());
         System.out.println(yoga.getDifficulty());
-
-        DoProteinShake delicious = new DoProteinShake(10);
+        DoProteinShake delicious = new DoProteinShake(10 , "Protein");
         delicious.setVolume(50);
         System.out.println(delicious);
         System.out.println(delicious.getPrice());
-
+        PrivateTraining bodyBuilding = new PrivateTraining(200 , "Bodybuilding" , "hard");
+        System.out.println(bodyBuilding);
+        System.out.println(Service.increaseServicePriceBy20(yoga));
+        System.out.println(Service.increaseServicePriceBy20(bodyBuilding));
+        System.out.println(Service.increaseServicePriceBy20(delicious));
     }
 }
