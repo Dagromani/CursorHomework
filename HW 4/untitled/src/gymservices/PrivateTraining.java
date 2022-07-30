70d6ce5f329014c4d6b8c162bfd39abe6f5aa2ce
Dagromani <109410300+Dagromani@users.noreply.github.com>
2022-07-30 17:00:04 +0300

    Update PrivateTraining.java

--- HW 4/untitled/src/gymservices/PrivateTraining.java
+++ HW 4/untitled/src/gymservices/PrivateTraining.java
@@ -1,36 +1,31 @@
 package gymservices;
 
-public class PrivateTraining extends Training implements Difficultyable{
-
-    private String difficulty;
-
+public class PrivateTraining extends Training {
+    public PrivateTraining(int price , String name , String difficulty) {
+        super(price, name , difficulty);
+    }
+    @Override
+    public String getName() {
+        return super.getName();
+    }
     @Override
     public int getPrice() {
         return super.getPrice();
     }
-
     @Override
     public void setDuration(int duration) {
         super.setDuration(duration);
     }
-
-    public PrivateTraining(int price, String difficulty) {
-        super(price);
-        this.difficulty = difficulty;
-    }
-
     @Override
     public int getDuration() {
         return super.getDuration();
     }
-
     @Override
     public String toString() {
         return super.toString();
     }
-
     @Override
     public String getDifficulty() {
-        return difficulty;
+        return super.getDifficulty();
     }
 }
