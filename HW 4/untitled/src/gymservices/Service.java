469e7a4d2badc51c925fbccbccb8c104d91253ee
Dagromani <109410300+Dagromani@users.noreply.github.com>
2022-08-02 14:42:31 +0300

    Update Service.java

--- HW 4/untitled/src/gymservices/Service.java
+++ HW 4/untitled/src/gymservices/Service.java
@@ -3,25 +3,24 @@ package gymservices;
 public abstract class Service {
     private int price;
     private String name;
+
     protected Service(int price , String name) {
         this.price = price;
         this.name = name;
     }
+
     public int getPrice() {
         return price;
     }
+
     public String getName() {
         return name;
     }
-    public static String increaseServicePriceBy20(GroupTraining service){
+
+    public static String increaseServicePriceBy20(Service service){
         return service.getName() + " now costs :" + (service.getPrice() + 20);
     }
-    public static String increaseServicePriceBy20(PrivateTraining service){
-        return service.getName() + " now costs :" + (service.getPrice() + 20);
-    }
-    public static String increaseServicePriceBy20(DoProteinShake service){
-        return service.getName() + " now costs :" + (service.getPrice() + 20);
-    }
+
     @Override
     public String toString() {
         return "Service{" +
