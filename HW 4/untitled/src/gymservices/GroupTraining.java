a39c372e0c1fdde4497f66bcccafe4b219b67684
Dagromani <109410300+Dagromani@users.noreply.github.com>
2022-08-02 14:41:34 +0300

    Update GroupTraining.java

--- HW 4/untitled/src/gymservices/GroupTraining.java
+++ HW 4/untitled/src/gymservices/GroupTraining.java
@@ -4,30 +4,34 @@ public class GroupTraining extends Training {
     public GroupTraining(int price, String name, String difficulty) {
         super(price, name, difficulty);
     }
+
     @Override
     public String getName() {
         return super.getName();
     }
+
     @Override
     public int getPrice() {
         return super.getPrice();
     }
+
     @Override
     public void setDuration(int duration) {
         super.setDuration(duration);
     }
+
     @Override
     public int getDuration() {
         return super.getDuration();
     }
+
     @Override
     public String toString() {
         return super.toString();
     }
+
     @Override
     public String getDifficulty() {
         return super.getDifficulty();
-    }
-}
     }
 }
