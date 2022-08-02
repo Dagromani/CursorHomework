180e05e0f0ea33a35ff1606e13bfc9e8accc320f
Dagromani <109410300+Dagromani@users.noreply.github.com>
2022-08-02 14:42:05 +0300

    Update PrivateTraining.java

--- HW 4/untitled/src/gymservices/PrivateTraining.java
+++ HW 4/untitled/src/gymservices/PrivateTraining.java
@@ -4,26 +4,32 @@ public class PrivateTraining extends Training {
     public PrivateTraining(int price , String name , String difficulty) {
         super(price, name , difficulty);
     }
+
     @Override
     public String getName() {
         return super.getName();
     }
+
     @Override
     public int getPrice() {
         return super.getPrice();
     }
+
     @Override
     public void setDuration(int duration) {
         super.setDuration(duration);
     }
+
     @Override
     public int getDuration() {
         return super.getDuration();
     }
+
     @Override
     public String toString() {
         return super.toString();
     }
+
     @Override
     public String getDifficulty() {
         return super.getDifficulty();
