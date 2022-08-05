718d46fe440bb46b28dbf6a1bdaf5b2641c07c57
Dagromani <109410300+Dagromani@users.noreply.github.com>
2022-08-05 23:03:06 +0300

    Update Main.java

--- HW5/src/Task3/Main.java
+++ HW5/src/Task3/Main.java
@@ -4,18 +4,13 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-
         ArrayList array = new ArrayList<>();
         array.add(1);
         array.add(2);
         array.add(-5);
         array.add(6);
-
-
         Gene example = new Gene(array);
         System.out.println("Highest number in array: " + example.highest());
         System.out.println("Lowest number in array: " + example.lowest());
-
-
     }
 }
