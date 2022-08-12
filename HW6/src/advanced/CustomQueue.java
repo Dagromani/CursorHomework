6196a9e303a394eae024b99419b086b16837c82d
Dagromani <109410300+Dagromani@users.noreply.github.com>
2022-08-12 15:36:32 +0300

    Add files via upload

---
+++ HW6/src/advanced/CustomQueue.java
@@ -0,0 +1,27 @@
+package advanced;
+
+public class CustomQueue extends CustomLinkedList{
+
+    public void enqueue(Object ele) {
+        super.addLast(ele);
+    }
+
+    public void dequeue() {
+        super.removeFirst();
+    }
+
+    @Override
+    public void printList() {
+        super.printList();
+    }
+
+    @Override
+    public int getSize() {
+        return super.getSize();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return super.isEmpty();
+    }
+}
---
+++ HW6/src/advanced/CustomStack.java
@@ -0,0 +1,41 @@
+package advanced;
+
+public class CustomStack<T> extends CustomSinglyLinkedList<T> {
+
+    public void push(T element) {
+        super.add(element);
+    }
+
+    public void pop() {
+        super.removeTail();
+    }
+
+    public T top() {
+        return super.getLast();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return super.isEmpty();
+    }
+
+    @Override
+    public T getFirst() {
+        return super.getFirst();
+    }
+
+    @Override
+    public int getSize() {
+        return super.getSize();
+    }
+
+    @Override
+    public void getElement(int index) {
+        super.getElement(index);
+    }
+
+    @Override
+    public void printList() {
+        super.printList();
+    }
+}
