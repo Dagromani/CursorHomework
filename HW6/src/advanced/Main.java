7e1935b4c84a13d7473a4537a36ba65592539aa9
Dagromani <109410300+Dagromani@users.noreply.github.com>
2022-08-14 16:26:25 +0300

    Update Main.java

--- HW6/src/advanced/Main.java
+++ HW6/src/advanced/Main.java
@@ -8,13 +8,12 @@ public class Main {
 
 //        CustomSinglyLinkedList<String> example = new CustomSinglyLinkedList<>();
 //        example.add("aaa");
-//        example.add("bbb");
-//        example.add("fsd");
-//        example.add("iii");
-//        example.add("ggg");
+//        System.out.println(example.getLast());
 //        example.getElement(0);
-//        example.getElement(3);
-//        example.getElement(4);
+
+//        CustomLinkedList linkList = new CustomLinkedList();
+//        linkList.add(0 , 7);
+//        System.out.println(linkList.getLast());
 
 //        CustomStack stack = new CustomStack();
 //        stack.push(6);
@@ -25,52 +24,42 @@ public class Main {
 //        System.out.println(stack.getSize());
 //        System.out.println("");
 //        stack.printList();
+//        System.out.println("");
 //        stack.pop();
 //        stack.pop();
 //        System.out.println("");
 //        stack.printList();
-//        System.out.println("");
-//        System.out.println(stack.top());
-//        System.out.println("");
-//        System.out.println(stack.getSize());
-//        System.out.println("");
-//        System.out.println(stack.isEmpty());
-//        System.out.println("");
-//        stack.pop();
-//        stack.pop();
-//        stack.pop();
-//        System.out.println(stack.isEmpty());
 
-        CustomLinkedList linkList = new CustomLinkedList();
-        linkList.addLast(7);
-        linkList.addLast(56);
-        linkList.addLast(23);
-        linkList.addLast(76);
-        linkList.addLast(999);
-        linkList.printList();
-        System.out.println(linkList.getSize());
-        System.out.println("");
-        linkList.addFirst(666);
-        linkList.addFirst(333);
-        linkList.printList();
-        System.out.println(linkList.getSize());
-        System.out.println("");
-        linkList.removeFirst();
-        linkList.add(0 , 888);
-        linkList.add(4 , 444);
-        linkList.printList();
 
-//        CustomQueue example = new CustomQueue();
-//        example.enqueue(8);
-//        example.enqueue(1);
-//        example.enqueue(3);
-//        example.enqueue(4);
-//        example.printList();
+//        CustomLinkedList linkList = new CustomLinkedList();
+//        linkList.addLast(7);
+//        linkList.addLast(56);
+//        linkList.addLast(23);
+//        linkList.addLast(76);
+//        linkList.addLast(999);
+//        linkList.printList();
+//        System.out.println(linkList.getSize());
 //        System.out.println("");
-//        example.printList();
-//        System.out.println(example.isEmpty());
-//        example.dequeue();
-//        example.dequeue();
-//        example.printList();
+//        linkList.addFirst(666);
+//        linkList.addFirst(333);
+//        linkList.printList();
+//        System.out.println(linkList.getSize());
+//        System.out.println("");
+//        linkList.removeFirst();
+//        linkList.add(0 , 888);
+//        linkList.add(4 , 444);
+//        linkList.printList();
+
+        CustomQueue example = new CustomQueue();
+        example.enqueue(8);
+        example.enqueue(1);
+        example.enqueue(3);
+        example.enqueue(4);
+        example.printList();
+        System.out.println("");
+        System.out.println(example.isEmpty());
+        example.dequeue();
+        example.dequeue();
+        example.printList();
     }
 }
